5fc26dca42e17027dd697e2632fbfbe6146a4a81
EquaCoin <30560198+EquaCoin@users.noreply.github.com>
2026-06-24 12:36:33 +0200

    Add remaining source files

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { registerServiceWorker } from './utils/notifications';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+registerServiceWorker();
